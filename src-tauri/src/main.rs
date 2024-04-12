cc2ed53842c173f9269194bee400df35e37e1e08
shiroedev <shiroedev@proton.me>
2024-04-12 13:48:55 +0330

    add basic commands

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,9 +7,37 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn start_leaf(rt_id: u16, config: &str) -> i32 {
+    let opts = leaf::StartOptions {
+        config: leaf::Config::Str(config.to_string()),
+        runtime_opt: leaf::RuntimeOption::SingleThread,
+    };
+    if let Err(e) = leaf::start(rt_id, opts) {
+        eprintln!("failed to start leaf: {}", e);
+        return 1;
+    }
+    return 0;
+}
+
+#[tauri::command]
+fn reload_leaf(rt_id: u16) -> i32 {
+    if let Err(e) = leaf::reload(rt_id) {
+        eprintln!("failed to reload leaf: {}", e);
+        return 1;
+    }
+    return 0;
+}
+
+#[tauri::command]
+fn stop_leaf(rt_id: u16) -> bool {
+    leaf::shutdown(rt_id)
+}
+
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![start_leaf, reload_leaf, stop_leaf])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/components/Greet.vue
+++ src/components/Greet.vue
@@ -1,21 +1,30 @@
 <script setup lang="ts">
-import { ref } from "vue";
 import { invoke } from "@tauri-apps/api/tauri";
+import { ref } from "vue";
 
-const greetMsg = ref("");
-const name = ref("");
+const config = ref("");
+const result = ref(0);
 
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsg.value = await invoke("greet", { name: name.value });
+async function start_leaf(runtime_id) {
+  result.value = await invoke("start_leaf", { runtime_id, config: config.value });
+}
+
+async function reload_leaf(runtime_id) {
+  result.value = await invoke("reload_leaf", { runtime_id });
+}
+
+async function stop_leaf(runtime_id) {
+  result.value = await invoke("stop_leaf", { runtime_id });
 }
 </script>
 
 <template>
-  <form class="row" @submit.prevent="greet">
-    <input id="greet-input" v-model="name" placeholder="Enter a name..." />
-    <button type="submit">Greet</button>
+  <form class="row" @submit.prevent="start_leaf">
+    <textarea id="config" v-model="config" placeholder="Leaf Config..." />
+    <button type="submit">Start</button>
+    <button @click="reload_leaf">Reload</button>
+    <button @click="stop_leaf">Stop</button>
   </form>
 
-  <p>{{ greetMsg }}</p>
+  <p>{{ result }}</p>
 </template>
