be9b88d83a95468089399bff8b1616c16ec68235
shiroedev <shiroedev@proton.me>
2024-04-23 18:20:56 +0330

    add doh section

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,82 +1,215 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use serde::Serialize;
-use tauri::Runtime;
-use std::{fs::File, io::{self, BufRead, BufReader}, thread};
+use std::io::BufRead;
+use std::net::{IpAddr, SocketAddr};
+use std::sync::{Arc, Mutex, MutexGuard};
+
+use serde::{Deserialize, Serialize};
+use tauri::{Runtime, Window};
 
 #[derive(Clone, Serialize)]
 enum State {
-    #[serde(rename = "running")]
-    Running,
-    #[serde(rename = "start_success")]
-    StartSuccess,
-    #[serde(rename = "start_error")]
-    StartError,
-    #[serde(rename = "stop_success")]
-    StopSuccess,
-    #[serde(rename = "stop_error")]
-    StopError,
-    #[serde(rename = "not_running")]
-    NotRunning
+    #[serde(rename = "leaf_running")]
+    LeafRunning,
+    #[serde(rename = "start_leaf_success")]
+    StartLeafSuccess,
+    #[serde(rename = "start_leaf_error")]
+    StartLeafError,
+    #[serde(rename = "stop_leaf_success")]
+    StopLeafSuccess,
+    #[serde(rename = "stop_leaf_error")]
+    StopLeafError,
+    #[serde(rename = "leaf_not_running")]
+    LeafNotRunning,
+    #[serde(rename = "doh_running")]
+    DohRunning,
+    #[serde(rename = "start_doh_success")]
+    StartDohSuccess,
+    #[serde(rename = "start_doh_error")]
+    StartDohError,
+    #[serde(rename = "stop_doh_success")]
+    StopDohSuccess,
+    #[serde(rename = "stop_doh_error")]
+    StopDohError,
+    #[serde(rename = "doh_not_running")]
+    DohNotRunning,
 }
 
 #[derive(Clone, Serialize)]
-struct LeafResult {
+struct Result {
     state: State,
-    message: Option<String>
+    message: Option<String>,
 }
 
-fn send_event<R: Runtime>(window: tauri::Window<R>, state: State, message: Option<String>) {
-    window.emit("leaf_event", LeafResult {
+#[derive(Clone, Serialize, Deserialize, Debug)]
+struct Doh {
+    listen: String,
+    server: String,
+    domain: String,
+    path: String,
+    post: bool,
+    fragment: bool,
+    fragment_packets: String,
+    fragment_lengths: String,
+    fragment_intervals: String,
+}
+
+fn send_event<R: Runtime>(window: MutexGuard<Window<R>>, state: State, message: Option<String>) {
+    window.emit("custom_event", Result {
         state,
         message,
     }).unwrap();
 }
 
 #[tauri::command]
-async fn run_leaf<R: Runtime>(_app: tauri::AppHandle<R>, window: tauri::Window<R>, config: String) {
-    thread::spawn(move || {
+fn start<R: Runtime>(_app: tauri::AppHandle<R>, window: Window<R>, leaf_config: String, doh_config: String) {
+    let window = Arc::new(Mutex::new(window));
+
+    let window_clone = window.clone();
+    tauri::async_runtime::spawn_blocking(move || {
+        if !doh::is_doh_running() {
+            let doh_config: Doh = serde_json::from_str(doh_config.as_str()).unwrap();
+
+            let listen_config = doh::ListenConfig::Addr(doh_config.listen.parse().unwrap());
+
+            let server: SocketAddr = doh_config.server.parse().unwrap();
+            let ip = match server.ip() {
+                IpAddr::V4(v4) => v4.to_string(),
+                IpAddr::V6(v6) => v6.to_string(),
+            };
+            let port = server.port();
+
+            let remote_config = if doh_config.fragment {
+                let (packets_from, packets_to) = parse_fragment_option(doh_config.fragment_packets).unwrap();
+                let (length_min, length_max) = parse_fragment_option(doh_config.fragment_lengths).unwrap();
+                let (interval_min, interval_max) = parse_fragment_option(doh_config.fragment_intervals).unwrap();
+
+                doh::RemoteHost::Fragment(
+                    ip,
+                    port,
+                    packets_from,
+                    packets_to,
+                    length_min,
+                    length_max,
+                    interval_min,
+                    interval_max,
+                )
+            } else {
+                doh::RemoteHost::Direct(ip, port)
+            };
+
+            let doh_config = doh::Config::new(
+                listen_config,
+                remote_config,
+                doh_config.domain.as_str(),
+                None,
+                None,
+                doh_config.path.as_str(),
+                1,
+                10,
+                doh_config.post,
+                0,
+                false,
+            ).unwrap();
+
+            match doh::run_doh(doh_config) {
+                Ok(_) => send_event(window_clone.lock().unwrap(), State::StopDohSuccess, None),
+                Err(e) => send_event(window_clone.lock().unwrap(), State::StartDohError, Some(e.to_string())),
+            }
+        } else {
+            println!("doh already running");
+        };
+    });
+
+    let window_clone = window.clone();
+    tauri::async_runtime::spawn_blocking(move || {
         if !leaf::is_running(0) {
             let options = leaf::StartOptions {
-                config: leaf::Config::Str(config),
+                config: leaf::Config::Str(leaf_config),
                 runtime_opt: leaf::RuntimeOption::SingleThread,
             };
             match leaf::start(0, options) {
-                Ok(_) => send_event(window, State::StopSuccess, None),
-                Err(e) => send_event(window, State::StartError, Some(e.to_string())),
+                Ok(_) => send_event(window_clone.lock().unwrap(), State::StopLeafSuccess, None),
+                Err(e) => send_event(window_clone.lock().unwrap(), State::StartLeafError, Some(e.to_string())),
             };
+        } else {
+            println!("leaf already running");
         };
     });
 }
 
 #[tauri::command]
-async fn stop_leaf<R: Runtime>(_app: tauri::AppHandle<R>, window: tauri::Window<R>) {
-    thread::spawn(move || {
+fn stop<R: Runtime>(_app: tauri::AppHandle<R>, window: Window<R>) {
+    let window = Arc::new(Mutex::new(window));
+
+    let window_clone = window.clone();
+    tauri::async_runtime::spawn_blocking(move || {
+        if doh::is_doh_running() {
+            if doh::shutdown_doh().is_err() {
+                send_event(window_clone.lock().unwrap(), State::StopDohError, None);
+            }
+        } else {
+            println!("doh not running");
+        }
+    });
+
+    let window_clone = window.clone();
+    tauri::async_runtime::spawn_blocking(move || {
         if leaf::is_running(0) {
             if !leaf::shutdown(0) {
-                send_event(window, State::StopError, None);
-            };
-        };
-    });
-}
-
-#[tauri::command]
-async fn is_leaf_running<R: Runtime>(_app: tauri::AppHandle<R>, window: tauri::Window<R>) {
-    thread::spawn(move || {
-        if leaf::is_running(0) {
-            send_event(window, State::Running, None);
+                send_event(window_clone.lock().unwrap(), State::StopLeafError, None);
+            }
         } else {
-            send_event(window, State::NotRunning, None);
+            println!("leaf not running");
         }
     });
 }
 
+#[tauri::command]
+fn is_leaf_running<R: Runtime>(_app: tauri::AppHandle<R>, window: Window<R>) {
+    let window = Arc::new(Mutex::new(window));
+
+    let window_clone = window.clone();
+    tauri::async_runtime::spawn_blocking(move || {
+        if leaf::is_running(0) {
+            send_event(window_clone.lock().unwrap(), State::LeafRunning, None);
+        } else {
+            send_event(window_clone.lock().unwrap(), State::LeafNotRunning, None);
+        }
+    });
+}
+
+#[tauri::command]
+fn is_doh_running<R: Runtime>(_app: tauri::AppHandle<R>, window: Window<R>) {
+    let window = Arc::new(Mutex::new(window));
+
+    let window_clone = window.clone();
+    tauri::async_runtime::spawn_blocking(move || {
+        if doh::is_doh_running() {
+            send_event(window_clone.lock().unwrap(), State::DohRunning, None);
+        } else {
+            send_event(window_clone.lock().unwrap(), State::DohNotRunning, None);
+        }
+    });
+}
+
+fn parse_fragment_option(option: String) -> Option<(u64, u64)> {
+    let parts: Vec<&str> = option.split('-').map(str::trim).collect();
+    if parts.len() == 2 {
+        let start = parts[0].parse::<u64>().ok()?;
+        let end = parts[1].parse::<u64>().ok()?;
+        Some((start, end))
+    } else {
+        None
+    }
+}
+
 fn main() {
-    #[cfg(any(target_os="linux", target_os="macos"))]
+    #[cfg(any(target_os = "linux", target_os = "macos"))]
     sudo::escalate_if_needed().unwrap();
 
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![run_leaf, stop_leaf, is_leaf_running])
+        .invoke_handler(tauri::generate_handler![start, stop, is_leaf_running, is_doh_running])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/components/Leaf.vue
+++ src/components/Leaf.vue
@@ -1,6 +1,7 @@
 <template>
-  <v-textarea v-model="config" full-width label="Config"></v-textarea>
-  <v-btn @click="running ? stopLeaf() : runLeaf()" :loading=loading>{{ running ? "Stop" : "Start" }}</v-btn>
+  <v-textarea v-model="leaf_config" full-width label="Leaf Config"></v-textarea>
+  <v-textarea v-model="doh_config" label="Doh Config"></v-textarea>
+  <v-btn @click="is_leaf_running && is_doh_running ? stop() : start()" :loading=loading>{{ is_leaf_running && is_doh_running ? "Stop" : "Start" }}</v-btn>
 </template>
 
 <script lang="ts">
@@ -11,15 +12,21 @@ import { appWindow } from "@tauri-apps/api/window";
 import { defineComponent } from 'vue';
 
 enum State {
-  Running = "running",
-  StartSuccess = "start_success",
-  StartError = "start_error",
-  StopSuccess = "stop_success",
-  StopError = "stop_error",
-  NotRunning = "not_running"
+  LeafRunning = "leaf_running",
+  StartLeafSuccess = "start_leaf_success",
+  StartLeafError = "start_leaf_error",
+  StopLeafSuccess = "stop_leaf_success",
+  StopLeafError = "stop_leaf_error",
+  LeafNotRunning = "leaf_not_running",
+  DohRunning = "doh_running",
+  StartDohSuccess = "start_doh_success",
+  StartDohError = "start_doh_error",
+  StopDohSuccess = "stop_doh_success",
+  StopDohError = "stop_doh_error",
+  DohNotRunning = "doh_not_running",
 }
 
-interface LeafResult {
+interface Result {
   state: State;
   message?: string | null;
 }
@@ -29,81 +36,120 @@ export default defineComponent({
   data() {
     return {
       loading: false,
-      running: false,
-      config: ""
+      is_leaf_running: false,
+      is_doh_running: false,
+      leaf_config: "",
+      doh_config: "",
     };
   },
   async created() {
-    this.loadConfig();
+    this.loadConfigs();
     await this.startListening();
-    await this.waitForWindows();
     await this.isLeafRunning();
+    await this.isDohRunning();
+    await this.waitForWindows();
   },
   watch: {
-    config(newConfig, _oldConfig) {
-      console.log("newConfig", newConfig);
-      this.saveConfig(newConfig);
+    leaf_config(newConfig, _oldConfig) {
+      console.log("newLeafConfig", newConfig);
+      this.saveLeafConfig(newConfig);
+    },
+    doh_config(newConfig, _oldConfig) {
+      console.log("newDohConfig", newConfig);
+      this.saveDohConfig(newConfig);
     }
   },
   methods: {
     async startListening() {
-      await listen('leaf_event', async (event: any) => {
+      await listen('custom_event', async (event: any) => {
         console.log(event);
 
-        const payload = event.payload as LeafResult;
+        const payload = event.payload as Result;
         const state: State = payload.state;
 
         switch (state) {
-          case State.Running:
-            this.running = true;
+          case State.LeafRunning:
+            this.is_leaf_running = true;
             break;
-          case State.NotRunning:
-            this.running = false;
+          case State.LeafNotRunning:
+            this.is_leaf_running = false;
             break;
 
-          case State.StartSuccess:
+          case State.StartLeafSuccess:
             break;
-          case State.StopSuccess:
-            this.running = false;
+          case State.StopLeafSuccess:
+            this.is_leaf_running = false;
             break;
-          case State.StartError:
-            this.running = false;
+          case State.StartLeafError:
+            this.is_leaf_running = false;
             await message(payload.message ?? "Unexpected error");
             break;
-          case State.StopError:
+          case State.StopLeafError:
             break;
-        };
+
+          case State.DohRunning:
+            this.is_doh_running = true;
+            break;
+          case State.DohNotRunning:
+            this.is_doh_running = false;
+            break;
+
+          case State.StartDohSuccess:
+            break;
+          case State.StopDohSuccess:
+            this.is_doh_running = false;
+            break;
+          case State.StartDohError:
+            this.is_doh_running = false;
+            await message(payload.message ?? "Unexpected error");
+            break;
+          case State.StopDohError:
+            break;
+        }
 
         this.loading = false;
       })
     },
     async waitForWindows() {
       await appWindow.onCloseRequested(async (event) => {
-        if (this.running) {
-          await message("Leaf is running. Please stop it first.");
+        if (this.is_leaf_running || this.is_doh_running) {
+          await message("Leaf or Doh is running. Please stop it first.");
           event.preventDefault();
         }
       });
     },
-    async runLeaf() {
+    async start() {
       this.loading = true;
-      await invoke("run_leaf", { config: this.config });
+      await invoke("start", { leafConfig: this.leaf_config , dohConfig: this.doh_config});
+
       setTimeout(async () => {
         await this.isLeafRunning();
+        console.log("checking for leaf running");
+      }, 1000);
+      setTimeout(async () => {
+        await this.isDohRunning();
+        console.log("checking for doh running");
       }, 1000);
     },
-    async stopLeaf() {
+    async stop() {
       this.loading = true;
-      await invoke("stop_leaf");
+      await invoke("stop");
     },
     async isLeafRunning() {
       await invoke("is_leaf_running");
     },
-    loadConfig() {
-      this.config = window.localStorage.getItem("config") || "";
+    async isDohRunning() {
+      await invoke("is_doh_running");
     },
-    saveConfig(newConfig: string) {
-      window.localStorage.setItem("config", newConfig);
+    loadConfigs() {
+      this.leaf_config = window.localStorage.getItem("leaf_config") || "";
+      this.doh_config = window.localStorage.getItem("doh_config") || "";
+    },
+    saveLeafConfig(newConfig: string) {
+      window.localStorage.setItem("leaf_config", newConfig);
+    },
+    saveDohConfig(newConfig: string) {
+      window.localStorage.setItem("doh_config", newConfig);
     }
   }
 });
