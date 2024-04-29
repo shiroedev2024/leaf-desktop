e100812f42cf8e2bd4877434c5ed30777a21ba7d
shiroedev <shiroedev@proton.me>
2024-04-29 12:40:34 +0330

    remove doh client

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,8 +7,6 @@ use tauri::{Runtime, Window};
 
 #[derive(Serialize, Clone)]
 enum AppState {
-    #[serde(rename = "start_doh_success")]
-    StartDohSuccess,
     #[serde(rename = "start_leaf_success")]
     StartLeafSuccess,
 }
@@ -16,100 +14,6 @@ enum AppState {
 #[derive(Serialize, Clone)]
 struct AppEvent {
     state: AppState,
-}
-
-#[derive(Deserialize)]
-struct DohConfig {
-    listen: SocketAddr,
-    server: SocketAddr,
-    domain: String,
-    path: String,
-    post: bool,
-    fragment: bool,
-    fragment_packets: String,
-    fragment_lengths: String,
-    fragment_intervals: String,
-}
-
-#[tauri::command]
-async fn run_doh<R: Runtime>(
-    _app: tauri::AppHandle<R>,
-    window: Window<R>,
-    doh_config: String,
-) -> Result<(), String> {
-    tauri::async_runtime::spawn_blocking(move || {
-        let doh_config: DohConfig = serde_json::from_str(doh_config.as_str())
-            .map_err(|e| format!("failed to parse doh config: {}", e))?;
-        let listen_config = doh::ListenConfig::Addr(doh_config.listen);
-
-        let ip = match doh_config.server.ip() {
-            IpAddr::V4(v4) => v4.to_string(),
-            IpAddr::V6(v6) => v6.to_string(),
-        };
-        let port = doh_config.server.port();
-
-        let remote_config = if !doh_config.fragment {
-            doh::RemoteHost::Direct(ip, port)
-        } else {
-            let (packets_from, packets_to) = parse_fragment_option(doh_config.fragment_packets)?;
-            let (length_min, length_max) = parse_fragment_option(doh_config.fragment_lengths)?;
-            let (interval_min, interval_max) =
-                parse_fragment_option(doh_config.fragment_intervals)?;
-
-            doh::RemoteHost::Fragment(
-                ip,
-                port,
-                packets_from,
-                packets_to,
-                length_min,
-                length_max,
-                interval_min,
-                interval_max,
-            )
-        };
-
-        let doh_config = doh::Config::new(
-            listen_config,
-            remote_config,
-            doh_config.domain.as_str(),
-            None,
-            None,
-            doh_config.path.as_str(),
-            1,
-            10,
-            doh_config.post,
-            0,
-            false,
-        )
-        .map_err(|e| format!("failed to create doh config: {}", e))?;
-
-        window
-            .emit(
-                "app_event",
-                AppEvent {
-                    state: AppState::StartDohSuccess,
-                },
-            )
-            .map_err(|e| format!("failed to emit app_event: {}", e))?;
-
-        if let Err(e) = doh::run_doh(doh_config) {
-            Err(format!("failed to start doh: {}", e))
-        } else {
-            Ok(())
-        }
-    })
-    .await
-    .map_err(|e| format!("failed to start doh: {}", e))?
-}
-
-#[tauri::command]
-fn is_doh_running<R: Runtime>(_app: tauri::AppHandle<R>, _window: Window<R>) -> bool {
-    doh::is_doh_running()
-}
-
-#[tauri::command]
-fn stop_doh<R: Runtime>(_app: tauri::AppHandle<R>, _window: Window<R>) -> bool {
-    doh::shutdown_doh().is_ok()
 }
 
 #[tauri::command]
@@ -169,27 +73,9 @@ fn stop_leaf<R: Runtime>(_app: tauri::AppHandle<R>, _window: Window<R>) -> bool 
     leaf::shutdown(0)
 }
 
-fn parse_fragment_option(option: String) -> Result<(u64, u64), String> {
-    let parts: Vec<&str> = option.split('-').map(str::trim).collect();
-    if parts.len() == 2 {
-        let start = parts[0]
-            .parse::<u64>()
-            .map_err(|_| "failed to parse as u64")?;
-        let end = parts[1]
-            .parse::<u64>()
-            .map_err(|_| "failed to parse as u64")?;
-        Ok((start, end))
-    } else {
-        Err("invalid fragment option".to_string())
-    }
-}
-
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
-            run_doh,
-            is_doh_running,
-            stop_doh,
             run_leaf,
             reload_leaf,
             is_leaf_running,
--- src/components/Leaf.vue
+++ src/components/Leaf.vue
@@ -1,7 +1,4 @@
 <template>
-  <v-textarea v-model="doh_config" label="Doh Config"></v-textarea>
-  <v-btn @click="doh_running ? stopDoh() : runDoh()">{{ doh_running ? "Stop DoH" : "Start DoH" }}</v-btn>
-
   <v-textarea v-model="leaf_config" full-width label="Leaf Config"></v-textarea>
   <v-btn @click="leaf_running ? stopLeaf() : runLeaf()">{{ leaf_running ? "Stop Leaf" : "Start Leaf" }}</v-btn>
 </template>
@@ -14,7 +11,6 @@ import { appWindow } from "@tauri-apps/api/window";
 import { defineComponent } from 'vue';
 
 enum AppState {
-  StartDohSuccess = "start_doh_success",
   StartLeafSuccess = "start_leaf_success",
 }
 
@@ -27,13 +23,12 @@ export default defineComponent({
   data() {
     return {
       leaf_running: false,
-      doh_running: false,
       leaf_config: "",
-      doh_config: "",
     };
   },
   async created() {
-    this.loadConfigs();
+    this.loadConfig();
+    await this.isLeafRunning();
     await this.startListening();
     await this.waitForWindows();
   },
@@ -42,10 +37,6 @@ export default defineComponent({
       console.log("newLeafConfig", newConfig);
       this.saveLeafConfig(newConfig);
     },
-    doh_config(newConfig, _oldConfig) {
-      console.log("newDohConfig", newConfig);
-      this.saveDohConfig(newConfig);
-    }
   },
   methods: {
     async startListening() {
@@ -53,44 +44,25 @@ export default defineComponent({
         let appEvent = event.payload as AppEvent;
         console.log(appEvent);
 
-        if (appEvent.state === AppState.StartDohSuccess) {
-          this.doh_running = true;
-        } else if (appEvent.state === AppState.StartLeafSuccess) {
+        if (appEvent.state === AppState.StartLeafSuccess) {
           this.leaf_running = true;
         }
       });
     },
     async waitForWindows() {
       await appWindow.onCloseRequested(async (event) => {
-        if (this.leaf_running || this.doh_running) {
-          await message("Leaf or Doh is running. Please stop it first.");
+        if (this.leaf_running) {
+          await message("Leaf is running. Please stop it first.");
           event.preventDefault();
         }
       });
-    },
-    async runDoh() {
-      try {
-        await invoke("run_doh", { dohConfig: this.doh_config });
-      } catch (e) {
-        console.error(e);
-        this.doh_running = false;
-      }
-    },
-    async isDohRunning() {
-      this.doh_running = await invoke("is_doh_running");
-    },
-    async stopDoh() {
-      const result = await invoke("stop_doh");
-      if (result) {
-        this.doh_running = false;
-      }
     },
     async runLeaf() {
       try {
         await invoke("run_leaf", { leafConfig: this.leaf_config });
       } catch (e) {
-        console.error(e);
         this.leaf_running = false;
+        await message("Error: " + e);
       }
     },
     async reloadLeaf() {
@@ -110,16 +82,12 @@ export default defineComponent({
         this.leaf_running = false;
       }
     },
-    loadConfigs() {
+    loadConfig() {
       this.leaf_config = window.localStorage.getItem("leaf_config") || "";
-      this.doh_config = window.localStorage.getItem("doh_config") || "";
     },
     saveLeafConfig(newConfig: string) {
       window.localStorage.setItem("leaf_config", newConfig);
     },
-    saveDohConfig(newConfig: string) {
-      window.localStorage.setItem("doh_config", newConfig);
-    }
   }
 });
 </script>
