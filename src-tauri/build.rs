5be0a90b2724eee0e23054dc9053f8fcf7be29e8
shiroedev2024 <shiroedev@proton.me>
2026-03-15 02:28:21 +0330

    update to 1.8.0

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -12,7 +12,7 @@ fn prepare_sidecar() -> Result<(), Box<dyn std::error::Error>> {
     LeafBuilder::new()
         .cache_dir("./sidecar_cache")
         .output_dir("./bin")
-        .version("0.7.8")
+        .version("0.7.9")
         .build()?;
     Ok(())
 }
