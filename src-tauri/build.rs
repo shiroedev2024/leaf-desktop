f6a005e0d96c6ac363f93cb965eabf34628e9bb3
shiroedev2024 <shiroedev@proton.me>
2026-04-12 11:03:26 +0330

    update to 1.9.7

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -12,7 +12,7 @@ fn prepare_sidecar() -> Result<(), Box<dyn std::error::Error>> {
     LeafBuilder::new()
         .cache_dir("./sidecar_cache")
         .output_dir("./bin")
-        .version("1.1.5")
+        .version("1.1.6")
         .build()?;
     Ok(())
 }
